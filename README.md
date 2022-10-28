1da0c25d20132919e6185896581dd5732d0d8742
Naylson Costa <naylsonrj@gmail.com>
2022-10-28 15:34:38 -0300

    projeto finalizado com readme

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">Welcome to API REST EXPRESS 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+</p>
+
+> API REST EXPRESS UTILIZANDO AUTENTICAÇÃO COM BLIOTECA BCRYPT, KNEX, JSONWEBTOKEN
+
+## Install
+
+```sh
+npm install
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **NAYLSON COSTA**
+
+* Website: www.linkedin.com/in/naylsoncosta
+* Github: [@naylsonrj](https://github.com/naylsonrj)
+
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,17 @@ var express = require("express")
 var app = express();
 var router = express.Router();
 var HomeController = require("../controllers/HomeController");
+var UserController = require("../controllers/UserController");
+var AdminAuth = require("../middleware/AdminAuth");
 
 router.get('/', HomeController.index);
+router.post('/user', UserController.create); // ok
+router.get("/user",AdminAuth,UserController.index);
+router.get("/user/:id",AdminAuth,UserController.findUser);
+router.put("/user",AdminAuth,UserController.edit);
+router.delete("/user/:id",AdminAuth,UserController.remove);
+router.post("/recoverpassword",UserController.recoverPassword);
+router.post("/changepassword",UserController.changePassword);
+router.post("/login",UserController.login);
 
 module.exports = router;
